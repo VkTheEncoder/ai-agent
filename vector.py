d48e23cb54849d7327d266068bdbbe7bb57282ba
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-08-05 23:45:39 +0530

    Update vector.py

--- vector.py
+++ vector.py
@@ -1,6 +1,6 @@
 # vector.py
 
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
 import os
